4e2a9681cd07aa9142e8944328acb78607b6a928
ryhan.chowdhury3@gmail.com <ryhan.chowdhury3@gmail.com>
2018-06-03 02:02:01 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,17 @@
 def unsafe?(speed)
-  if speed < 40 || speed > 60 ? "unsafe" : "safe"
+  if speed < 40 || speed > 60
+    return "unsafe"
+  else
+    return "safe"
+  end
 
 end
 
 
 
 def not_safe?(speed)
-	
+  if speed < 40 || speed > 60 ? "unsafe" : "safe"
+	end
 end
 	
 
